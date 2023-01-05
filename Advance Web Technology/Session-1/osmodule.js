353983d0392b77f4a70fd410b09b00bf25feead1
TheNobody-12 <sarthak.kce20@sot.pdpu.ac.in>
2023-01-05 11:54:28 +0530

    added osmodule.js

---
+++ Advance Web Technology/Session-1/osmodule.js
@@ -0,0 +1,7 @@
+const os = require('os');
+console.log(os.freemem());
+console.log(os.homedir());
+console.log(os.hostname());
+console.log(os.version());
+console.log(os.machine());
+console.log(os.totalmem());
