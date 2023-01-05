87e341354a057df9cd8ac9767bce9c7ea02fcc68
TheNobody-12 <sarthak.kce20@sot.pdpu.ac.in>
2023-01-05 11:48:59 +0530

    Task1 completed

---
+++ Advance Web Technology/Session-1/index.js
@@ -0,0 +1,4 @@
+const g2 = require('./second');
+console.log('-----------------------------------------------------------------------------')
+console.log('hello from index.js');
+console.log(g2);
---
+++ Advance Web Technology/Session-1/second.js
@@ -0,0 +1,7 @@
+deva = {
+    name: "Sarthak",
+    age: 20,
+    developer: true
+}
+module.exports = deva;
+console.log(exports, require, module, __filename, __dirname);
